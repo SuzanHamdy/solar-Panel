5c8ad5262306095b47f397c28f9a5a699583ab3c
SuzanHamdy <150383354+SuzanHamdy@users.noreply.github.com>
2024-09-04 16:03:33 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var div12 =document.getElementById("div12");
+var responsive= document.getElementById("responsive");
+var i= document.getElementById("i");
+var a;
+
+
+ div12.onclick = ()=>{
+if (    a==1) {
+        responsive.style.display ='none';
+         return a=0
+} else {
+    responsive.style.display ='block';
+         return a=1
+}
+}
+i.onclick =()=>{
+    responsive.style.display ='none';
+}
+
